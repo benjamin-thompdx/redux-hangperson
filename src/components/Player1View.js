ffd0f3d036b5567d78e18e647b2ea3e6b9d34d1b
Benjamin Thom <jaminthom@gmail.com>
2020-04-29 12:02:11 -0700

    update src/components/Player1View.js
    Co-authored-by: K. Wicz <katy@solovewicz.com>
    Co-authored-by: Mariam <maralaeddin@gmail.com>
    Co-authored-by: Jonathan Carlos <jonathancarlos21@hotmail.com>

--- src/components/Player1View.js
+++ src/components/Player1View.js
@@ -1,2 +1,14 @@
 import React from "react";
 import ReusableForm from './ReusableForm';
+import Card from 'react-bootstrap/Form';
+
+
+function Player1View() {
+  <React.Fragment>
+    <Card>
+      <ReusableForm label="Write a word or phrase for your opponent to guess."></ReusableForm>
+    </Card>
+  </React.Fragment>
+}
+
+export default Player1View;

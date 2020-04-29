988542ec2d8b6ca1119f881d2c440c50d65e829e
Benjamin Thom <jaminthom@gmail.com>
2020-04-29 14:11:07 -0700

    update src/components/app.js to include updated Header.js component
    Co-authored-by: K. Wicz <katy@solovewicz.com>
    Co-authored-by: Mariam <maralaeddin@gmail.com>
    Co-authored-by: Jonathan Carlos <jonathancarlos21@hotmail.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from './Header';
 import Player1View from './Player1View';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <React.Fragment>
+      <Header />
       <Player1View />
     </React.Fragment>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from 'react-bootstrap/Form';
+
+
+function Header() {
+  return (
+    <React.Fragment>
+      <Jumbotron fluid>
+        <h1>HANGPERSON</h1>
+      </Jumbotron>
+    </React.Fragment>
+  )
+}

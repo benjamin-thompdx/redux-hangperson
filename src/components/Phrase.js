3b1ec586ded8a22157c6b12dbbf5cb382426a04f
Benjamin Thom <jaminthom@gmail.com>
2020-04-30 11:40:01 -0700

    update components to include src/components/Phrase.js
    Co-authored-by: K. Wicz <katy@solovewicz.com>
    Co-authored-by: Mariam <maralaeddin@gmail.com>
    Co-authored-by: Jonathan Carlos <jonathancarlos21@hotmail.com>

---
+++ src/components/Phrase.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from "prop-types";
+
+
+function Phrase(props) {
+
+  return (
+    <React.Fragment>
+      <p>{props.player1Input}</p>
+    </React.Fragment>
+  );
+}
+
+Phrase.propTypes = {
+  player1Input: PropTypes.string
+};
+
+export default Phrase;

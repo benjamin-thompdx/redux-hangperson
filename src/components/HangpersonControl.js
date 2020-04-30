0a4cba33de22a2befbfb051e8e391ccd241290b4
Benjamin Thom <jaminthom@gmail.com>
2020-04-30 10:06:24 -0700

    update src/components/HangpersonControl.js
    Co-authored-by: K. Wicz <katy@solovewicz.com>
    Co-authored-by: Mariam <maralaeddin@gmail.com>
    Co-authored-by: Jonathan Carlos <jonathancarlos21@hotmail.com>

---
+++ src/components/HangpersonControl.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Player1View from './Player1View';
+import Player2View from './Player2View/Player2View';
+
+class HangpersonControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedView: null
+    };
+  }
+
+  render(){
+    return (
+      <React.Fragment>
+
+      </React.Fragment>
+    );
+  }
+}
+
+
+export default HangpersonControl;

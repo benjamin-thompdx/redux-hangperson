e3d89eb9e0e5fc5b4eedbe18f207c4bff4a18676
Benjamin Thom <jaminthom@gmail.com>
2020-04-30 10:52:55 -0700

    update src/components/Player1View.js to include handleNewGameCreation handler method and pass through props to be used in other components
    Co-authored-by: K. Wicz <katy@solovewicz.com>
    Co-authored-by: Mariam <maralaeddin@gmail.com>
    Co-authored-by: Jonathan Carlos <jonathancarlos21@hotmail.com>

--- src/components/Player1View.js
+++ src/components/Player1View.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React from 'react';
 import ReusableForm from './ReusableForm';
+import PropTypes from 'prop-types';
 import Card from 'react-bootstrap/Card';
 
 
 function Player1View() {
+
+  function handleNewGameCreation(event) {
+    event.preventDefault();
+    props.onNewGameCreation({
+      phrase: event.target.phrase.value
+    });
+  }
+
   return (
   <React.Fragment>
     <Card>
-      <ReusableForm label="Write a word or phrase for your opponent to guess."></ReusableForm>
+      <ReusableForm 
+        label="Write a word or phrase for your opponent to guess."
+        formSubmissionHandler={handleNewGameCreation}>    
+      </ReusableForm>
     </Card>
   </React.Fragment>
   );
 }
+
+Player1View.propTypes = {
+  onNewGameCreation: PropTypes.func
+};
 
 export default Player1View;
